3d897ae137efecd4b44e5873cb9bf2cf561d6867
SangND <dacsang97@gmail.com>
2017-09-22 01:09:41 +0700

    Upload to git

---
+++ README.md
@@ -0,0 +1,23 @@
+# Egg Toggle
+
+> An Egg Toggle Project
+
+## Use
+- [Vue.js](https://vuejs.org/)
+- [GSAP TweenMax](https://greensock.com/tweenmax)
+- [BodyMovin](https://github.com/bodymovin/bodymovin)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div id="egg-toggle" ref="egg" @click="onClicked"></div>
+  </div>
+</template>
+
+<script>
+import eggFlip from './assets/egg_flip.json'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      anim: null
+    }
+  },
+  mounted () {
+    this.anim = bodymovin.loadAnimation({
+        wrapper: this.$refs.egg,
+        animType: 'svg',
+        loop: false,
+        prerender: false,
+        autoplay: false,
+        animationData: eggFlip
+      });
+    this.anim.setSpeed(15);
+  },
+  methods: {
+    onClicked () {
+      if (this.anim.currentFrame > 0) {
+        this.anim.playSegments([this.anim.currentFrame, 0], true);
+        TweenMax.to('.eggGroup', 1, {
+          x:0,
+          ease:Elastic.easeOut.config(0.9,0.38)
+        })   
+      } else {
+        TweenMax.to('.eggGroup', 1.4, {
+          x:73,
+          ease:Elastic.easeOut.config(0.9,0.38)
+        })
+        this.anim.playSegments([this.anim.currentFrame, 300], true)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body, #app {
+ background-color: #29AFB0;
+ overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+body,
+html {
+ height: 100%;
+ width: 100%;
+ margin: 0;
+ padding: 0;
+}
+
+#egg-toggle {
+ width: 50%;;
+ height: 50%;
+ -webkit-tap-highlight-color:transparent;
+
+ cursor:pointer;
+}
+</style>
